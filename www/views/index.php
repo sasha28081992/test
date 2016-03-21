aa87947d99d45f038bddb1604dad8a47149ac791
sasha28081992 <sasha28081992@mail.ru>
2016-03-21 17:25:43 +0300

    Отображение всех новостей

--- www/views/index.php
+++ www/views/index.php
@@ -1,7 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Alex
- * Date: 21.03.2016
- * Time: 17:08
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Новости</title>
+</head>
+<body>
+    <?php
+    foreach ($items as $item) :?>
+
+        <br><img src="<?php echo $item['path'];?>" style="max-width: 300px">
+        <br><?php echo $item['title']; ?>
+
+    <?php endforeach; ?>
+</body>
+</html>
