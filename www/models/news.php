b96e7c849544424dde4008559e439bbae8b8863f
sasha28081992 <sasha28081992@mail.ru>
2016-03-22 11:38:36 +0300

    Выборка списка всех новостей

--- www/models/news.php
+++ www/models/news.php
@@ -3,6 +3,6 @@ require __DIR__ . '/../functions/sql.php';
 
 function News_getAll() {
     Sql_connect();
-    $sql = 'SELECT * FROM news;';
+    $sql = 'SELECT * FROM news ORDER BY id DESC';
     return Sql_query($sql);
 }
